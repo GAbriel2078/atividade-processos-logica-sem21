97f9178cbbf4692a79c8b0bb6eeac7c3e9adc8f3
GAbriel2078 <00001118491105SP@al.educacao.sp.gov.br>
2025-10-06 10:06:09 -0300

    Add files via upload

---
+++ .py
@@ -0,0 +1,41 @@
+class ContaBancaria:
+    def __init__(self, numero_conta, saldo=0):
+        self.__numero_conta = numero_conta
+        self.__saldo = saldo
+        self.__transacoes = []
+def depositar(self, valor):
+    self.__saldo += valor
+    self.__registrar_transacao("Depósito", valor)
+def sacar(self, valor):
+    if self.__saldo >= valor:
+       self.__saldo -= valor
+       self.__registrar_transacao("Saque", valor)
+    else:  
+         print("Saldo insuficiente.")
+def consultar_saldo(self):
+    print("Saldo:", self.saldo)
+def registrar_transacao(self, tipo, valor):
+    self.__transacoes.append({"Tipo": tipo, "Valor": valor})
+
+class ContaCorrente(ContaBancaria):
+    def __init__(self, numero_conta, limite_cheque_especial=0):
+        super().__init__(numero_conta)
+        self.limite_cheque_especial = limite_cheque_especial
+def emitir_cheque(self, valor):
+    if self.__saldo + self.limite_cheque_especial >= valor:
+       self.__saldo -= valor
+       self.__registrar_transacao("Emissão de Cheque", valor)
+    else:
+         print("Limite de cheque especial excedido.")
+
+class ContaPoupanca(ContaBancaria):
+    def calcular_juros_mensal(self, taxa_juros):
+        juros = self.saldo * (taxa_juros / 100)
+        self.__saldo += juros
+        self.__registrar_transacao("Juros Mensais", juros)
+class ContaInvestimento(ContaBancaria):
+    def __init__(self, numero_conta):
+        super().__init__(numero_conta)
+        self.__investimentos = []
+def realizar_investimento(self, produto, valor):
+    self.__registrar_transacao("Investimento", valor)
